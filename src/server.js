d3f6742a843e0a64921de5d4e7950a3d6d23dacf
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2022-03-09 11:24:12 +0100

    Try fix

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const url = dev ? "/" : "captiva"; // https://dev.to/agustinl/deploy-sapper-pwa-on-github-pages-2ih1
+
+polka()
+  .use(
+    url, // Ajout.
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
