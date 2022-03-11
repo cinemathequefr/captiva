b66a427052d4e48562d01c4abb09e309c63197bd
nltesown <nlte@magic.fr>
2022-03-11 13:51:55 +0100

    Minor edit

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ import * as sapper from "@sapper/server";
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-const url = "/captiva"; // https://dev.to/agustinl/deploy-sapper-pwa-on-github-pages-2ih1
+const url = dev ? "/" : "/captiva"; // https://dev.to/agustinl/deploy-sapper-pwa-on-github-pages-2ih1
+// const url = "/captiva"; // https://dev.to/agustinl/deploy-sapper-pwa-on-github-pages-2ih1
 
 polka()
   .use(
