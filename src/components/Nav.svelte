4d2e23c3cf64e17c65b5cddc3ac7678028df74f8
NLTESOWN <nlte@fastmail.com>
2024-02-12 11:40:48 +0100

    Mise à jour id progs

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,7 +3,7 @@
   import { token } from "../stores/token.js";
   import { global } from "../stores/global.js";
 
-  const progs = [152, 158, 163, 165];
+  const progs = [158, 163, 165, 179];
   // const progs = [124, 129, 143, 146, 150, 152];
 
   // 2022-12-15 : On boucle sur la liste des programmes disponibles (`progs`). Si le store $global.currentProgId n'a pas de valeur, on prend le dernier programme disponible.
