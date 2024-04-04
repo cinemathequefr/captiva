bcf6b483af3bb280fd488912f2a0d2c9a3db61ef
nltesown <nltesown@users.noreply.github.com>
2024-04-04 10:35:39 +0000

    deploy: b51a0d4ee3c8de02a6a0907ed3eca2146efe7b86

--- client/index.cb37310a.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as a,h as e,d as c,n as i}from"./client.c551d47b.js";function n(s){return{c(){this.h()},l(s){e("svelte-marxjf",document.head).forEach(c),this.h()},h(){document.title="Captiva"},m:i,p:i,i:i,o:i,d:i}}class o extends s{constructor(s){super(),t(this,s,null,n,a,{})}}export{o as default};
