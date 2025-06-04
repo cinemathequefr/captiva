205eec1797f3f9ea545ef8c3b45bd10da6272f98
NLTESOWN <nlte@fastmail.com>
2025-06-04 11:32:40 +0200

    Revert "Réduit automatiquement la hauteur d'un textarea au blur quand c'est possible"
    This reverts commit 6f39a83ef20263321828bc2ba5e1d3200fb37d96.

--- src/components/lib/Form.svelte
+++ src/components/lib/Form.svelte
@@ -16,15 +16,6 @@
         let t = textareas[i];
         updateTextareaHeight(t);
         t.addEventListener("input", () => updateTextareaHeight(t));
-
-        t.addEventListener("blur", () => {
-          if (t.style.height !== "") {
-            setTimeout(() => {
-              t.style.height = "";
-              updateTextareaHeight(t);
-            }, 10);
-          }
-        });
       }
 
       function updateTextareaHeight(t) {
